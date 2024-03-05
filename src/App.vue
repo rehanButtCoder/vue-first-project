b895e5b90d0eff2571f5d18f8eb1bdc821d26338
Butt, Rehan Butt <rehan.butt15555@gmail.com>
2024-03-05 09:58:31 +0500

    things to be done after installation

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          | <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+#layout{
+  text-align: center;
+}
+nav{
+  padding: 30px;
+}
+nav a{
+  font-weight: bold;
+}
+nav a.router-link-exact-active{
+  color: red;
+}
+
+</style>
