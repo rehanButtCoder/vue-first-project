02c9f02dcac6c33b9a609fdd2fa16cf0405feb72
Butt, Rehan Butt <rehan.butt15555@gmail.com>
2024-03-05 10:24:04 +0500

    in progress

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,34 @@
 <script setup>
 
+const event = ref(
+    {
+        id:1,
+        category : "animal welfare",
+        title : "animal",
+        des : "animal welfare",
+        category : "animal welfare",
+        category : "animal welfare",
+        category : "animal welfare",
+        category : "animal welfare",
+        category : "animal welfare",
+    }
+)
 </script>
 
 <template>
- 
+ <div class="event-card"></div>
 </template>
 
 <style scoped>
-
+.event-card{
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid;
+    margin-bottom: 18px;
+}
+.event-card:hover{
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0,0,0,0.2);
+}
 </style>
